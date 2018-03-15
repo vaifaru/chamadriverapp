59de36489d3a8d477cd91a7058bb277d00ea6548
Rodrigo Henriques <rodrigo.henriques@99taxis.com>
2018-03-15 15:52:49 -0300

    Home presenter test class initial setup

--- app/src/test/kotlin/br/com/chamaapp/driver/features/home/HomePresenterTest.kt
+++ app/src/test/kotlin/br/com/chamaapp/driver/features/home/HomePresenterTest.kt
@@ -1,15 +1,31 @@
 package br.com.chamaapp.driver.features.home
 
+import br.com.chamaapp.driver.api.DriverApi
+import br.com.chamaapp.driver.infra.di.module.SchedulersComposer
+import br.com.chamaapp.driver.services.LocationService
 import org.junit.Test
 import org.mockito.Mock
 
 class HomePresenterTest {
 
   @Mock
+  lateinit var view: HomeContract.View
+
+  @Mock
+  lateinit var locationService: LocationService
+
+  @Mock
+  lateinit var api: DriverApi
+
+  @Mock
+  lateinit var schedulersComposer: SchedulersComposer
+
+  @Mock
   lateinit var presenter: HomePresenter
 
   @Test
   fun `actionClicks$production_sources_for_module_app`() {
+    val testObserver = presenter.actionClicks().test()
   }
 
   @Test
